308e11139e7fc993f18f35e6f727cc72af082d9b
nstark03 <46936668+nstark03@users.noreply.github.com>
2020-05-11 20:23:31 -0500

    Updated the java code to be up to date with the c++
    It travels, it seeks the lEMoN

---
+++ java-multiCameraServer/src/main/java/LemonPipeline.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+import org.opencv.core.*;
+import org.opencv.imgproc.*;
+
+import edu.wpi.first.vision.VisionPipeline;
+
+public class LemonPipeline implements VisionPipeline {
+    public static int val;
+    public static Mat drawing;
+    public static double tx;
+    public static double ty;
+
+    @Override
+    public void process(Mat mat) {
+        val++;
+
+        Mat proc = new Mat();
+  
+        Imgproc.cvtColor(mat, proc, Imgproc.COLOR_BGR2HSV);
+  
+        //inRange(proc, Scalar(0, 127, 127), Scalar(15, 255, 255), proc); //tracks red
+        Core.inRange(proc, new Scalar(22, 95, 0), new Scalar(30, 255, 255), proc);
+  
+        //blur(proc, proc, Size(4, 4));
+  
+        //threshold(proc, proc, 150, 255, 0);
+  
+        Imgproc.erode(proc, proc, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5)));
+        Imgproc.dilate(proc, proc, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5)));
+  
+        Imgproc.dilate(proc, proc, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5)));
+        Imgproc.erode(proc, proc, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5)));
+  
+        Imgproc.Canny(proc, proc, 100, 200, 3, false);
+  
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+  
+        Imgproc.findContours(proc, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+  
+        MatOfPoint2f[] contoursPoly = new MatOfPoint2f[contours.size()];
+        Rect boundRects[] = new Rect[contours.size()];
+  
+        for (int i = 0; i < contours.size(); i++) {
+            contoursPoly[i] = new MatOfPoint2f();
+            Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
+            boundRects[i] = Imgproc.boundingRect(contoursPoly[i]);
+        }
+  
+        Imgproc.cvtColor(proc, proc, Imgproc.COLOR_GRAY2BGR);
+  
+        //for (int i = 0; i < contours.size(); i++) {
+        //    drawContours(*dest, contours, i, Scalar(255, 0, 0), 2, 8, hierarchy, 0, Point());
+        //}
+        Rect bRect = new Rect();
+  
+        if (boundRects.length > 0) {
+            int maxIndex = 0;
+            for (int i = 0; i < boundRects.length; i++) {
+                maxIndex = (boundRects[i].area() > boundRects[maxIndex].area()) ? i : maxIndex;
+            }
+            Imgproc.rectangle(proc, boundRects[maxIndex].tl(), boundRects[maxIndex].br(), new Scalar(0, 0, 255));
+            bRect = boundRects[maxIndex];
+        }
+  
+        //double tx, ty; //pixels offset from center of image
+  
+        if (bRect.width != 0 && bRect.height != 0) {
+            tx = ((double)bRect.tl().x - (((double)proc.cols() / 2.0) - (double)bRect.width / 2.0));
+            ty = ((((double)proc.rows() / 2.0) - (double)bRect.height / 2.0) - (double)bRect.tl().y);
+            //cout << tx << ", " << ty << endl;
+        }
+  
+      
+      }
+}
--- java-multiCameraServer/src/main/java/MyPipeline.java
+++ java-multiCameraServer/src/main/java/MyPipeline.java
@@ -1,4 +1,3 @@
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.vision.VisionPipeline;
 
 import java.util.*;
@@ -7,68 +6,126 @@ import org.opencv.core.*;
 import org.opencv.imgproc.*;
 
 public class MyPipeline implements VisionPipeline {
-  public static int val;
-  // public static Mat hierarchy;
   public static Mat drawing;
+  public static int val;
+  public static double tx;
+  public static double ty;
 
   @Override
   public void process(Mat mat) {
-    // hierarchy = new Mat();
-    // drawing = new Mat();
-    val += 1;
-    // Mat grey = new Mat();
+    Mat proc = new Mat();
+
+    val++;
+    
     Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
-    // Imgcodecs.imwrite("/home/pi/photos/grey.jpg", grey);
+
     Imgproc.blur(mat, mat, new Size(3, 3));
-    // Mat bin = new Mat();
+
     Imgproc.threshold(mat, mat, 150, 255, 0);
-    // Imgcodecs.imwrite("/home/pi/photos/bin.jpg", bin);
-    // Mat edges = new Mat();
+
     Imgproc.Canny(mat, mat, 50, 200, 3, false);
+
     List<MatOfPoint> contours = new ArrayList<>();
     Mat hierarchy = new Mat();
+
     Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 
-    //calculate the bounding rectangles
     MatOfPoint2f[] contoursPoly = new MatOfPoint2f[contours.size()];
-    Rect bounds[] = new Rect[contours.size()];
+    Rect boundRects[] = new Rect[contours.size()];
+
     for (int i = 0; i < contours.size(); i++) {
       contoursPoly[i] = new MatOfPoint2f();
       Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
-      bounds[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
-    }
+      boundRects[i] = Imgproc.boundingRect(contoursPoly[i]);
+  }
 
-    //initialize matrix and list for holding the contours and shapes
-    drawing = Mat.zeros(mat.size(), CvType.CV_8UC3);
-    List<MatOfPoint> contoursPolyList = new ArrayList<>(contoursPoly.length);
-    
-    //add all of the bounding rectangles to a list
-    for (MatOfPoint2f poly : contoursPoly) {
-      contoursPolyList.add(new MatOfPoint(poly.toArray()));
-    }
+  Imgproc.cvtColor(proc, proc, Imgproc.COLOR_GRAY2BGR);
 
-    //draw the bounding rectangles and the contours - unused, currently draws the largest later on in the code
-    for (int i = 0; i < contours.size(); i++) {
-      Imgproc.drawContours(drawing, contoursPolyList, i, new Scalar(0, 255, 0));
-      // Imgproc.rectangle(drawing, bounds[i].tl(), bounds[i].br(), new Scalar(255, 0, 0), 2);
-    }
+  //for (int i = 0; i < contours.size(); i++) {
+  //    drawContours(*dest, contours, i, Scalar(255, 0, 0), 2, 8, hierarchy, 0, Point());
+  //}
+  Rect bRect = new Rect();
 
-    // Imgcodecs.imwrite("/home/pi/photos/drawing.jpg", drawing);
-
-    //find the index of the largest rect by area
-    if(bounds.length > 0) {
+  if (boundRects.length > 0) {
       int maxIndex = 0;
-      for(int i = 0; i < bounds.length; i++) {
-        maxIndex = bounds[i].area() > bounds[maxIndex].area() ? i : maxIndex;
+      for (int i = 0; i < boundRects.length; i++) {
+          maxIndex = (boundRects[i].area() > boundRects[maxIndex].area()) ? i : maxIndex;
       }
-      Imgproc.rectangle(drawing, bounds[maxIndex].tl(), bounds[maxIndex].br(), new Scalar(255, 0, 0), 2);
-      double height = bounds[maxIndex].height;
-      double width = bounds[maxIndex].width;
-      NetworkTableEntry areaEntry = Main.visionTable.getEntry("height");
-      NetworkTableEntry widthEntry = Main.visionTable.getEntry("width");
-      areaEntry.setDouble(height);
-      widthEntry.setDouble(width);
-    }
+      Imgproc.rectangle(proc, boundRects[maxIndex].tl(), boundRects[maxIndex].br(), new Scalar(0, 0, 255));
+      bRect = boundRects[maxIndex];
+  }
+
+  //double tx, ty; //pixels offset from center of image
+
+  if (bRect.width != 0 && bRect.height != 0) {
+      tx = ((double)bRect.tl().x - (((double)proc.cols() / 2.0) - (double)bRect.width / 2.0));
+      ty = ((((double)proc.rows() / 2.0) - (double)bRect.height / 2.0) - (double)bRect.tl().y);
+      //cout << tx << ", " << ty << endl;
+  }
+}
+
+  // public static int val;
+  // // public static Mat hierarchy;
+  // public static Mat drawing;
+
+  // @Override
+  // public void process(Mat mat) {
+  //   // hierarchy = new Mat();
+  //   // drawing = new Mat();
+  //   val += 1;
+  //   // Mat grey = new Mat();
+  //   Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
+  //   // Imgcodecs.imwrite("/home/pi/photos/grey.jpg", grey);
+  //   Imgproc.blur(mat, mat, new Size(3, 3));
+  //   // Mat bin = new Mat();
+  //   Imgproc.threshold(mat, mat, 150, 255, 0);
+  //   // Imgcodecs.imwrite("/home/pi/photos/bin.jpg", bin);
+  //   // Mat edges = new Mat();
+  //   Imgproc.Canny(mat, mat, 50, 200, 3, false);
+  //   List<MatOfPoint> contours = new ArrayList<>();
+  //   Mat hierarchy = new Mat();
+  //   Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+  //   //calculate the bounding rectangles
+  //   MatOfPoint2f[] contoursPoly = new MatOfPoint2f[contours.size()];
+  //   Rect bounds[] = new Rect[contours.size()];
+  //   for (int i = 0; i < contours.size(); i++) {
+  //     contoursPoly[i] = new MatOfPoint2f();
+  //     Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
+  //     bounds[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
+  //   }
+
+  //   //initialize matrix and list for holding the contours and shapes
+  //   drawing = Mat.zeros(mat.size(), CvType.CV_8UC3);
+  //   List<MatOfPoint> contoursPolyList = new ArrayList<>(contoursPoly.length);
+    
+  //   //add all of the bounding rectangles to a list
+  //   for (MatOfPoint2f poly : contoursPoly) {
+  //     contoursPolyList.add(new MatOfPoint(poly.toArray()));
+  //   }
+
+  //   //draw the bounding rectangles and the contours - unused, currently draws the largest later on in the code
+  //   for (int i = 0; i < contours.size(); i++) {
+  //     Imgproc.drawContours(drawing, contoursPolyList, i, new Scalar(0, 255, 0));
+  //     // Imgproc.rectangle(drawing, bounds[i].tl(), bounds[i].br(), new Scalar(255, 0, 0), 2);
+  //   }
+
+  //   // Imgcodecs.imwrite("/home/pi/photos/drawing.jpg", drawing);
+
+  //   //find the index of the largest rect by area
+  //   if(bounds.length > 0) {
+  //     int maxIndex = 0;
+  //     for(int i = 0; i < bounds.length; i++) {
+  //       maxIndex = bounds[i].area() > bounds[maxIndex].area() ? i : maxIndex;
+  //     }
+  //     Imgproc.rectangle(drawing, bounds[maxIndex].tl(), bounds[maxIndex].br(), new Scalar(255, 0, 0), 2);
+  //     double height = bounds[maxIndex].height;
+  //     double width = bounds[maxIndex].width;
+  //     NetworkTableEntry areaEntry = Main.visionTable.getEntry("height");
+  //     NetworkTableEntry widthEntry = Main.visionTable.getEntry("width");
+  //     areaEntry.setDouble(height);
+  //     widthEntry.setDouble(width);
+  //   }
     // Mat lines = new Mat();
     // Imgproc.HoughLinesP(edges, lines, 1, Math.PI/180, 150);
     // Point vertices[] = new Point[lines.rows()];
@@ -92,5 +149,5 @@ public class MyPipeline implements VisionPipeline {
     // 0);
     // }
     // Imgcodecs.imwrite("/home/pi/photos/coloredges.jpg", colorEdges);
-  }
+  
 }
